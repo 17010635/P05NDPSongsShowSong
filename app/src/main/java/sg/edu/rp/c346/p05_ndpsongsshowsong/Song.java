84ea1f6d8ebfb1915bfe03f05cdee9231798e5fa
17010635 <17010635@myrp.edu.sg>
2019-05-21 13:03:33 +0800

    commit

---
+++ app/src/main/java/sg/edu/rp/c346/p05_ndpsongsshowsong/Song.java
@@ -0,0 +1,38 @@
+package sg.edu.rp.c346.p05_ndpsongsshowsong;
+
+import java.io.Serializable;
+
+public class Song implements Serializable {
+    private int id;
+    private String title;
+    private String singers;
+    private int year;
+    private int star;
+
+    public Song(String title, String singers, int year, int star) {
+        this.title = title;
+        this.singers = singers;
+        this.year = year;
+        this.star = star;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSingers() {
+        return singers;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getStar() {
+        return star;
+    }
+}
