fed4cbe3885d93a5567a792f342198bd324d554d
17010635 <17010635@myrp.edu.sg>
2019-05-21 14:33:29 +0800

    commit

--- app/src/main/java/sg/edu/rp/c346/p05_ndpsongsshowsong/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/p05_ndpsongsshowsong/MainActivity.java
@@ -1,5 +1,6 @@
 package sg.edu.rp.c346.p05_ndpsongsshowsong;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -39,10 +40,19 @@ public class MainActivity extends AppCompatActivity {
                 int year = Integer.parseInt(etYear.getText().toString());
                 int star = Integer.parseInt(etYear.getText().toString());
                 db.insertSong(etSong.getText().toString(), etSingers.getText().toString(), year, star);
+                db.close();
             }
         });
 
+        btnShowList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getBaseContext(),  ShowActivity.class);
 
+                startActivity(i);
+            }
+        });
+        };
 
     }
-}
+
